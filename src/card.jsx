8acab13b9be1e072717e8cf2a99b155ab2ea5d7b
K41FET1 <166552308+K41FET1@users.noreply.github.com>
2024-10-08 12:08:10 +0400

    card.jsx

--- src/card.jsx
+++ src/card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Card({ name, desc, src, age, bg }) {
   return (
-    <div className="card" style={{ backgroundColor: bg }}>
+    <div className="card" style={{ backgroundColor: bg }}> 
       <img src={src} alt={name} />
       <h2>{name}</h2>
       <p>{desc}</p>
-      <p>Age: {age}</p>
+      <p>Age: {age}</p> 
     </div>
   );
 }
